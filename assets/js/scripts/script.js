078e0c085d1a9e34b99b31fd30c7371014fcf70c
apometricsTK <Tobias.Kast@t-online.de>
2020-12-07 11:47:17 +0000

    add d3 table gen, flatten list for easier access? github object [object Object]

--- assets/js/scripts/script.js
+++ assets/js/scripts/script.js
@@ -4,13 +4,10 @@ let cdnjsFullList = null;
 document.addEventListener("DOMContentLoaded", initSite);
 
 async function initSite() {
-    console.log("DOM fully loaded and parsed");
-
     cdnjsFullList = await fetchCdnjsLibrariesFullList();
 
-    console.log(typeof(cdnjsFullList));
+    generateTable();
 };
-
 
 // cdnjs api call with keyword and github values included
 async function fetchCdnjsLibrariesFullList() {
@@ -19,3 +16,44 @@ async function fetchCdnjsLibrariesFullList() {
         return result.results;
     };
 
+function generateTable() {
+    generateTableHead();
+    generateTableBody();
+};
+
+function generateTableHead() {
+    let keysOfList = Object.keys(cdnjsFullList[0]);
+    
+    d3.select("#mainTable")
+        .append("thead")
+        .append("tr")
+        .selectAll("th")
+        .data(keysOfList)
+        .enter()
+        .append("th")
+        .text(i => i)
+};
+
+function generateTableBody() { 
+    let columns = Object.keys(cdnjsFullList[0]);
+    
+    d3.select("#mainTable")
+        .append("tbody")
+        
+        //generate rows
+        .selectAll("tr")
+        .data(cdnjsFullList)
+        .enter()
+        .append("tr")
+
+        //generate cells
+        .selectAll("td")
+        .data(function(row) {
+            return columns.map(function(column) {
+                return {column: column, value: row[column]}
+            })
+        })
+        .enter()
+        .append("td")
+        .text(i => i.value);
+};
